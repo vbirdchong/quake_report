2cc74d6330d74c05d24501b09b78f1c547d79114
chongchen <cc.chong.chen@gmail.com>
2017-09-17 23:20:55 +0800

    implement the different color display for circle background for magnitude

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
+import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +40,10 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         TextView magTextView = (TextView) listItemView.findViewById(R.id.id_mag);
         magTextView.setText(getFormatMagnitude(currentEarehquake.getMagnitude()));
+
+        GradientDrawable magnitudeCircle = (GradientDrawable)  magTextView.getBackground();
+        int magnitudeColor = getMagnitudeColor(currentEarehquake.getMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
 
         String originalPlace = currentEarehquake.getPlace();
         String locationOffset;
@@ -81,6 +87,46 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private String getFormatMagnitude(double mag) {
         DecimalFormat magFormat = new DecimalFormat("0.0");
         return  magFormat.format(mag);
+    }
+
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
     }
 
     private String getLocationOffset(String placeInfo) {
