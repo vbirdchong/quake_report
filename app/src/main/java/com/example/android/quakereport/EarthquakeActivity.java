251547b8338e5ebc6fe952968b2b6de4afc52a7e
chongchen <cc.chong.chen@gmail.com>
2017-10-08 22:18:28 +0800

    add a menu to EarthquakeActivity

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -26,6 +26,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -157,5 +159,22 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
                 mAdapter.addAll(earthquakes);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
